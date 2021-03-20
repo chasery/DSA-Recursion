e8c139e9cf628fcc95d8e982323e90f2f6621baf
Ryan Chase <ryan@chasery.com>
2021-03-20 14:59:13 -0700

    Refactor of maze

--- maze-solver.js
+++ maze-solver.js
@@ -1,71 +1,50 @@
 /* Find a way out of the maze
 
-You have entered a Maze and need to find your way out of it. There are more than one possible paths through the Maze to the single exit point. Write a recursive function that will help you find a possible path through the maze.
+Part 1: You have entered a Maze and need to find your way out of it. There are more than one possible paths through the Maze to the single exit point. Write a recursive function that will help you find a possible path through the maze.
 
 The Maze is represented as a N*M matrix (in the above case, a 3X3 or a 5X7 array). The starting point is the top left corner and the exit is indicated by e. For simplicity purpose, use the bottom right corner of the maze as the exit. You can't go outside the boundaries of the maze. The maze has passages that are blocked and you can't go through them. These blocked passages are indicated by *. Passing through a blocked cell as well as passing though a cell that you have already passed before are forbidden.
 
 For the above maze, a possible exit path can be RRDDLLDDRRRRRR
 
+Part 2: Use the above maze and modify your solution so it finds All the possible exit paths through the Maze. To find all possible exit paths through the maze, think about how many places you can move at each turn. Possibly up, down, left or right?
+
+Notice that this maze has 3 exits paths. Your recursive function should print all three of the paths with the proper directions.
+
 What is the input to the program?
   A maze.
 What is the output of the program?
-  The directions of a path to get out at 'e.'
+  The paths to get out of the maze at 'e.'
 What is the input to each recursive call?
-  The maze, row, column, and lastMove
+  The maze, row, column, direction, and path.
 What is the output of each recursive call?
-  A direction to take.
+  A direction if we can move that direction.
 */
 
-const availableMoves = (maze, row, column, lastMove) => {
-  const moves = [];
-  // Check if coordinates stay within bounds of the maze and whether or not it has a wall
-  if (row - 1 >= 0 && maze[row - 1][column] !== '*' && lastMove !== 'D') {
-    moves.push('U');
+const mazeSolver = (maze, row = 0, column = 0, dir = '', path = '') => {
+  if (row < 0 || column < 0) {
+    return;
   }
-  if (
-    column + 1 < maze[row].length &&
-    maze[row][column + 1] !== '*' &&
-    lastMove !== 'L'
-  ) {
-    moves.push('R');
+  if (row >= maze.length) {
+    return;
   }
-  if (
-    row + 1 < maze.length &&
-    maze[row + 1][column] !== '*' &&
-    lastMove !== 'U'
-  ) {
-    moves.push('D');
+  if (column >= maze[0].length) {
+    return;
   }
-  if (column - 1 >= 0 && maze[row][column - 1] !== '*' && lastMove !== 'R') {
-    moves.push('L');
-  }
-  return moves;
-};
 
-const mazeSolver = (maze, row, column, lastMove) => {
-  row = row || 0;
-  column = column || 0;
-  // Returns an array of available moves
-  const moves = availableMoves(maze, row, column, lastMove);
-  // Base case
+  path += dir; // Store direction to current path
+
   if (maze[row][column] === 'e') {
-    return '';
-  } else {
-    // General case
-    for (const move of moves) {
-      if (move === 'U') {
-        return move + mazeSolver(maze, row - 1, column, move);
-      }
-      if (move === 'R') {
-        return move + mazeSolver(maze, row, column + 1, move);
-      }
-      if (move === 'D') {
-        return move + mazeSolver(maze, row + 1, column, move);
-      }
-      if (move === 'L') {
-        return move + mazeSolver(maze, row, column - 1, move);
-      }
-    }
+    console.log('Path to the exit: ' + path);
+    return;
+  }
+  // Check
+  if (maze[row][column] === ' ') {
+    maze[row][column] = '+'; // Fill current position to prevent infinite loop of going back and forth
+    mazeSolver(maze, row - 1, column, 'U', path);
+    mazeSolver(maze, row + 1, column, 'D', path);
+    mazeSolver(maze, row, column - 1, 'L', path);
+    mazeSolver(maze, row, column + 1, 'R', path);
+    maze[row][column] = ' '; // Clear current position
   }
 };
 
@@ -83,8 +62,10 @@ let maze = [
   [' ', ' ', ' ', ' ', ' ', ' ', 'e'],
 ];
 
-console.log(mazeSolver(maze));
+mazeSolver(maze);
 
 /* Result:
-  RRDDRRUURRDDDD
+  Path to the exit: RRDDLLDDRRRRRR
+  Path to the exit: RRDDRRUURRDDDD
+  Path to the exit: RRDDRRRRDD
 */
